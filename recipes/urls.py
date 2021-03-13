8b3ef51a163fcfdf0403fcb09096d7cd58a88e92
Максим Секачёв <cement-fan@ya.ru>
2021-03-13 07:43:52 +0300

    закрыл слэши и удалил неиспользуемые файлы

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,27 +6,28 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('recipe/<int:recipe_id>/', views.view_recipe, name='view_recipe'),
+    path('recipe/new/', views.add_recipe, name='add_recipe'),
     path('recipe/<int:recipe_id>/edit/', views.edit_recipe,
          name='edit_recipe'),
     path('recipe/<int:recipe_id>/delete/', views.del_recipe,
          name='del_recipe'),
-    path('favorite/', views.favorite, name='favorite'),
-    path('favorites/<int:recipe_id>', views.favorites, name='favorites'),
-
     path(
         'recipes/<str:username>/',
         views.authors_recipes,
         name='authors_recipes'
     ),
+
+    path('favorite/', views.favorite, name='favorite'),
+    path('favorites/<int:recipe_id>/', views.favorites, name='favorites'),
+
     path('follow/<str:username>/', views.author_follow, name='follow'),
     path('unfollow/<str:username>/', views.author_unfollow, name='unfollow'),
+
     path('subscriptions/', views.subscriptions, name='subscriptions'),
-    path('purchases/<int:recipe_id>', views.purchases, name='purchases'),
+    path('purchases/<int:recipe_id>/', views.purchases, name='purchases'),
     path('shoppinglist/', views.shopping_list, name='shopping_list'),
     path('shoppinglist/save/', views.shopping_list_save,
          name='shopping_list_save'),
-
-    path('add_recipe/', views.add_recipe, name='add_recipe'),
 
     path('ingredients/', views.ingredients, name='ingredients'),
 
